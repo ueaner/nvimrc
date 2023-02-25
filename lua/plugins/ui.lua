6a60ce15cb600eff6ce5b2d3e7c613615064d719
ueaner <ueaner@gmail.com>
2023-02-26 00:12:20 +0800

    feat(ui): add `<leader>tt` for toggle horizontal terminal

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -94,6 +94,19 @@ return {
     end,
   },
 
+  -- terminal
+  {
+    "NvChad/nvterm",
+    keys = {
+      -- stylua: ignore
+      -- `<Esc><Esc>` enter normal mode, then switch windows
+      { "<leader>tt", function() require("nvterm.terminal").toggle("horizontal") end, desc = "toggle term", mode = { "n", "t" } },
+    },
+    config = function()
+      require("nvterm").setup()
+    end,
+  },
+
   -- colorizer
   {
     "NvChad/nvim-colorizer.lua",
