c4552167ed0a2b8c91e91a7b044a6548046c82b0
ueaner <ueaner@gmail.com>
2023-03-24 00:30:19 +0800

    chore(terminal): make the floating terminal window bigger

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -9,7 +9,20 @@ return {
       { "<leader>tt", function() require("nvterm.terminal").toggle("horizontal") end, desc = "toggle term", mode = { "n", "t" } },
     },
     config = function()
-      require("nvterm").setup()
+      require("nvterm").setup({
+        terminals = {
+          type_opts = {
+            float = {
+              border = "single",
+              relative = "editor",
+              row = 0.05,
+              col = 0.05,
+              width = 0.9,
+              height = 0.82,
+            },
+          },
+        },
+      })
     end,
   },
 
