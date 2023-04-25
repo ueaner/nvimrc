6a55cb1b5917709229e4906fab6b41553b71e5fd
ueaner <ueaner@gmail.com>
2023-04-25 15:56:29 +0800

    feat(buf): add buffer force close parameter

--- lua/utils/buf.lua
+++ lua/utils/buf.lua
@@ -18,8 +18,11 @@ end
 --   outliner   0          0          nofile
 --   others     0          0          help | quickfix | prompt | acwrite | nowrite
 --
-M.close = function(bufnr)
+--- @param bufnr? integer see |bufnr()|, default: 0 for current.
+--- @param force? boolean force close
+M.close = function(bufnr, force)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
+  force = force or false
   -- non-buflisted
   if not vim.bo[bufnr].buflisted then
     if vim.bo[bufnr].buftype == "terminal" then
@@ -35,6 +38,12 @@ M.close = function(bufnr)
   -- buffer listed - File not modified or saved
   if not vim.bo[bufnr].modified then -- switch, close
     vim.cmd("bp | bd!" .. bufnr)
+    return
+  end
+
+  -- force close
+  if force then
+    vim.cmd("update | bp | bd" .. bufnr)
     return
   end
 
