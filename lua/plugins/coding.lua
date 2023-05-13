09f70d8818a99998389cefc0adacd539fefc1efb
ueaner <ueaner@gmail.com>
2023-05-14 01:11:39 +0800

    feat(yaml): add yaml support

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -43,6 +43,7 @@ return {
   -- language specific extension modules
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "plugins.extras.lang.yaml" },
   { import = "plugins.extras.lang.go" },
   { import = "plugins.extras.lang.lua" },
   { import = "plugins.extras.lang.php" },
---
+++ lua/plugins/extras/lang/yaml.lua
@@ -0,0 +1,30 @@
+local generate = require("plugins.extras.lang.spec").generate
+local nls = require("null-ls")
+
+---@type LangConfig
+local conf = {
+  ft = "yaml",
+  parsers = { -- nvim-treesitter: language parsers
+    "yaml",
+  },
+  cmdtools = { -- mason.nvim: cmdline tools for LSP servers, DAP servers, formatters and linters
+    "yaml-language-server",
+    "yamllint",
+  },
+  lsp = {
+    servers = { -- nvim-lspconfig: setup lspconfig servers
+      yamlls = {
+        settings = {
+          yaml = {
+            keyOrdering = false,
+          },
+        },
+      },
+    },
+    nls_sources = { -- null-ls.nvim: builtins formatters, diagnostics, code_actions
+      nls.builtins.diagnostics.yamllint,
+    },
+  },
+}
+
+return generate(conf)
