8f8b0f12f59a70e1acd38791ed1a7b73138d3d2b
ueaner <ueaner@gmail.com>
2023-06-05 19:37:06 +0800

    chore(lsp): clean code

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -50,7 +50,6 @@ return {
       -- LSP Server Settings
       ---@type lspconfig.options
       servers = {
-        jsonls = {},
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
           settings = {
