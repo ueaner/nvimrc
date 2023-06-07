1ce64e0ca72217ef0b40a87dd4a77c9d10bb3dd2
ueaner <ueaner@gmail.com>
2023-06-07 15:57:09 +0800

    chore: clean code

--- lua/plugins/editor/fuzzyfinder.lua
+++ lua/plugins/editor/fuzzyfinder.lua
@@ -22,7 +22,6 @@ return {
     cmd = "Telescope",
     version = false, -- telescope did only one release, so use HEAD for now
     keys = {
-      { "<leader>si", "<cmd>Telescope cheat fd<cr>", desc = "Cheatsheets" },
       { "<leader>,", "<cmd>Telescope buffers show_all_buffers=true<cr>", desc = "Switch Buffer" },
       { "<leader>/", U.telescope("live_grep"), desc = "Grep (root dir)" },
       { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
