4586e450920091aa31ec7d539a3d0fc86ecf720a
ueaner <ueaner@gmail.com>
2023-06-08 19:06:09 +0800

    chore: sync LazyVim configuration

--- lua/plugins/editor/trouble.lua
+++ lua/plugins/editor/trouble.lua
@@ -15,7 +15,10 @@ return {
           if require("trouble").is_open() then
             require("trouble").previous({ skip_groups = true, jump = true })
           else
-            vim.cmd.cprev()
+            local ok, err = pcall(vim.cmd.cprev)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
           end
         end,
         desc = "Previous trouble/quickfix item",
@@ -26,7 +29,10 @@ return {
           if require("trouble").is_open() then
             require("trouble").next({ skip_groups = true, jump = true })
           else
-            vim.cmd.cnext()
+            local ok, err = pcall(vim.cmd.cnext)
+            if not ok then
+              vim.notify(err, vim.log.levels.ERROR)
+            end
           end
         end,
         desc = "Next trouble/quickfix item",
