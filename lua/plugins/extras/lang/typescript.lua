aacd10eba7ce5226e7d22782f3abbf84ff42edd0
ueaner <ueaner@gmail.com>
2023-07-02 23:27:12 +0800

    chore(typescript): cleanup code

--- lua/plugins/extras/lang/typescript.lua
+++ lua/plugins/extras/lang/typescript.lua
@@ -60,14 +60,6 @@ return {
       },
       setup = {
         tsserver = function(_, opts)
-          require("utils").on_attach(function(client, buffer)
-            if client.name == "tsserver" then
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-            end
-          end)
           require("typescript").setup({ server = opts })
           return true
         end,
