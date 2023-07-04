1e3dc58f96f244b34ab2340213c27e2cfe18df06
ueaner <ueaner@gmail.com>
2023-07-04 13:34:26 +0800

    feat(lsp): added support to lsp keymaps for dynamic capabilities

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -90,6 +90,17 @@ return {
         require("plugins.lsp.keymaps").on_attach(client, buffer)
       end)
 
+      local register_capability = vim.lsp.handlers["client/registerCapability"]
+      vim.lsp.handlers["client/registerCapability"] = function(err, res, ctx)
+        local ret = register_capability(err, res, ctx)
+        local client_id = ctx.client_id
+        ---@type lsp.Client
+        local client = vim.lsp.get_client_by_id(client_id)
+        local buffer = vim.api.nvim_get_current_buf()
+        require("plugins.lsp.keymaps").on_attach(client, buffer)
+        return ret
+      end
+
       -- diagnostics
       for name, icon in pairs(require("config").icons.diagnostics) do
         name = "DiagnosticSign" .. name
