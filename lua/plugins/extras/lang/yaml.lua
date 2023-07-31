6c52279dbc6a8f3a553975792124e6849b94351b
ueaner <ueaner@gmail.com>
2023-07-31 21:19:10 +0800

    fix(yamlls): must disable built-in schemaStore support to use schemas from SchemaStore.nvim plugin

--- lua/plugins/extras/lang/yaml.lua
+++ lua/plugins/extras/lang/yaml.lua
@@ -15,8 +15,16 @@ local conf = {
     servers = { -- nvim-lspconfig: setup lspconfig servers
       yamlls = {
         settings = {
+          redhat = { telemetry = { enabled = false } },
           yaml = {
             keyOrdering = false,
+            format = { enable = true },
+            schemaStore = {
+              -- Must disable built-in schemaStore support to use schemas from SchemaStore.nvim plugin
+              enable = false,
+              -- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+              url = "",
+            },
           },
         },
       },
