5357382909b33a079f014d80401cc1e0ade4e748
ueaner <ueaner@gmail.com>
2023-10-07 19:43:15 +0800

    style(edgy): outline/database window moves from left to right

--- lua/plugins/extras/tools/edgy.lua
+++ lua/plugins/extras/tools/edgy.lua
@@ -8,119 +8,88 @@ return {
       { "<leader>ue", function() require("edgy").toggle() end, desc = "Edgy Toggle" },
       { "<leader>uE", function() require("edgy").select() end, desc = "Edgy Select Window" },
     },
-    opts = function()
-      local opts = {
-        ---@type table<Edgy.Pos, {size:integer | fun():integer, wo?:vim.wo}>
-        options = {
-          left = { size = 40 },
-          bottom = { size = 10 },
-          right = { size = 40 },
-          top = { size = 10 },
-        },
-        bottom = {
-          {
-            ft = "terminal",
-            title = "Terminal",
-            size = { height = 0.3 },
-            filter = function(buf, win)
-              return vim.api.nvim_win_get_config(win).relative == ""
-            end,
-          },
-          {
-            ft = "noice",
-            size = { height = 0.3 },
-            filter = function(buf, win)
-              return vim.api.nvim_win_get_config(win).relative == ""
-            end,
-          },
-          "Trouble",
-          { ft = "qf", title = "QuickFix" },
-          {
-            ft = "help",
-            size = { height = 0.3 },
-            -- don't open help files in edgy that we're editing
-            filter = function(buf)
-              return vim.bo[buf].buftype == "help"
-            end,
-          },
-          { ft = "spectre_panel", size = { height = 0.3 } },
-          { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 0.3 } },
-        },
-        left = {
-          {
-            title = "Explorer",
-            ft = "neo-tree",
-            filter = function(buf)
-              return vim.b[buf].neo_tree_source == "filesystem"
-            end,
-            pinned = true,
-            open = function()
-              vim.api.nvim_input(vim.g.mapleader .. "e") -- <leader>e
-            end,
-            size = { height = 0.5 },
-          },
-          { title = "Neotest Summary", ft = "neotest-summary" },
-
-          -- Use `>`/`<` to switch git_status/buffers sources on the neo-tree window
-          -- or use the telescope git_status/buffers command
-
-          -- {
-          --   title = "Git",
-          --   ft = "neo-tree",
-          --   filter = function(buf)
-          --     return vim.b[buf].neo_tree_source == "git_status"
-          --   end,
-          --   -- pinned = true,
-          --   open = "Neotree position=right git_status",
-          -- },
-          -- telescope buffers
-          -- {
-          --   title = "Buffers",
-          --   ft = "neo-tree",
-          --   filter = function(buf)
-          --     return vim.b[buf].neo_tree_source == "buffers"
-          --   end,
-          --   -- pinned = true,
-          --   open = "Neotree position=top buffers",
-          -- },
-        },
-        keys = {
-          -- increase width
-          ["<c-Right>"] = function(win)
-            win:resize("width", 2)
-          end,
-          -- decrease width
-          ["<c-Left>"] = function(win)
-            win:resize("width", -2)
-          end,
-          -- increase height
-          ["<c-Up>"] = function(win)
-            win:resize("height", 2)
-          end,
-          -- decrease height
-          ["<c-Down>"] = function(win)
-            win:resize("height", -2)
+    opts = {
+      ---@type table<Edgy.Pos, {size:integer | fun():integer, wo?:vim.wo}>
+      options = {
+        left = { size = 40 },
+        bottom = { size = 10 },
+        right = { size = 40 },
+        top = { size = 10 },
+      },
+      bottom = {
+        {
+          ft = "terminal",
+          title = "Terminal",
+          size = { height = 0.3 },
+          filter = function(buf, win)
+            return vim.api.nvim_win_get_config(win).relative == ""
           end,
         },
-      }
-      if require("utils").has("aerial.nvim") then
-        table.insert(opts.left, {
+        {
+          ft = "noice",
+          size = { height = 0.3 },
+          filter = function(buf, win)
+            return vim.api.nvim_win_get_config(win).relative == ""
+          end,
+        },
+        "Trouble",
+        { ft = "qf", title = "QuickFix" },
+        {
+          ft = "help",
+          size = { height = 0.3 },
+          -- don't open help files in edgy that we're editing
+          filter = function(buf)
+            return vim.bo[buf].buftype == "help"
+          end,
+        },
+        { ft = "spectre_panel", size = { height = 0.3 } },
+        { title = "Neotest Output", ft = "neotest-output-panel", size = { height = 0.3 } },
+      },
+      left = {
+        -- Use `>`/`<` to switch git_status/buffers sources on the neo-tree window
+        -- or use the telescope git_status/buffers commands
+        {
+          title = "Explorer",
+          ft = "neo-tree",
+          filter = function(buf)
+            return vim.b[buf].neo_tree_source == "filesystem"
+          end,
+          pinned = true,
+          open = function()
+            vim.api.nvim_input(vim.g.mapleader .. "e") -- <leader>e
+          end,
+          size = { height = 0.5 },
+        },
+        { title = "Neotest Summary", ft = "neotest-summary" },
+      },
+      right = {
+        {
           title = "Outline",
           ft = "aerial",
-          -- pinned = true,
-          open = "AerialToggle",
-        })
-      end
-
-      if require("utils").has("vim-dadbod-ui") then
-        table.insert(opts.left, {
+        },
+        {
           title = "Database",
           ft = "dbui",
-          -- pinned = true,
-          open = "DBUIToggle",
-        })
-      end
-      return opts
-    end,
+        },
+      },
+      keys = {
+        -- increase width
+        ["<c-Right>"] = function(win)
+          win:resize("width", 2)
+        end,
+        -- decrease width
+        ["<c-Left>"] = function(win)
+          win:resize("width", -2)
+        end,
+        -- increase height
+        ["<c-Up>"] = function(win)
+          win:resize("height", 2)
+        end,
+        -- decrease height
+        ["<c-Down>"] = function(win)
+          win:resize("height", -2)
+        end,
+      },
+    },
   },
 }
