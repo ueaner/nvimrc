49f9a877dbf861dc915ccd51ff2e88d13fd9fca8
ueaner <ueaner@gmail.com>
2023-10-08 19:08:44 +0800

    pref: delay notifications till vim.notify was replaced or after 500ms

--- lua/utils/lazier.lua
+++ lua/utils/lazier.lua
@@ -65,7 +65,46 @@ function M.lazy_file()
   })
 end
 
+-- delay notifications till vim.notify was replaced or after 500ms
+function M.lazy_notify()
+  local notifs = {}
+  local function temp(...)
+    table.insert(notifs, vim.F.pack_len(...))
+  end
+
+  local orig = vim.notify
+  vim.notify = temp
+
+  local timer = vim.loop.new_timer()
+  local check = assert(vim.loop.new_check())
+
+  local replay = function()
+    timer:stop()
+    check:stop()
+    if vim.notify == temp then
+      vim.notify = orig -- put back the original notify if needed
+    end
+    vim.schedule(function()
+      ---@diagnostic disable-next-line: no-unknown
+      for _, notif in ipairs(notifs) do
+        vim.notify(vim.F.unpack_len(notif))
+      end
+    end)
+  end
+
+  -- wait till vim.notify has been replaced
+  check:start(function()
+    if vim.notify ~= temp then
+      replay()
+    end
+  end)
+  -- or if it took more than 500ms, then something went wrong
+  timer:start(500, 0, replay)
+end
+
 function M.setup()
+  M.lazy_notify()
+
   M.add_lazy_file_event()
   M.lazy_file()
 end
