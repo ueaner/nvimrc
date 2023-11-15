2f0225762f614ad54a6765141be1108d9209daf1
ueaner <ueaner@gmail.com>
2023-11-16 02:11:24 +0800

    feat(explorer): added support for LSP renaming to neo-tree

--- lua/plugins/editor/explorer.lua
+++ lua/plugins/editor/explorer.lua
@@ -106,6 +106,18 @@ return {
       },
     },
     config = function(_, opts)
+      -- rename / move file
+      local function on_move(data)
+        require("utils.lsp").on_rename(data.source, data.destination)
+      end
+
+      local events = require("neo-tree.events")
+      opts.event_handlers = opts.event_handlers or {}
+      vim.list_extend(opts.event_handlers, {
+        { event = events.FILE_MOVED, handler = on_move },
+        { event = events.FILE_RENAMED, handler = on_move },
+      })
+
       require("neo-tree").setup(opts)
       vim.api.nvim_create_autocmd("TermClose", {
         pattern = "*lazygit",
