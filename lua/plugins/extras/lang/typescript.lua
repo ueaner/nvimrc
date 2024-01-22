f8ca6617bb2f31b8858098f3fcfce98ea7734ff8
ueaner <ueaner@gmail.com>
2024-01-22 17:21:01 +0800

    chore: clean code

--- lua/plugins/extras/lang/typescript.lua
+++ lua/plugins/extras/lang/typescript.lua
@@ -55,20 +55,6 @@ local conf = {
           },
         },
         settings = {
-          typescript = {
-            format = {
-              indentSize = vim.o.shiftwidth,
-              convertTabsToSpaces = vim.o.expandtab,
-              tabSize = vim.o.tabstop,
-            },
-          },
-          javascript = {
-            format = {
-              indentSize = vim.o.shiftwidth,
-              convertTabsToSpaces = vim.o.expandtab,
-              tabSize = vim.o.tabstop,
-            },
-          },
           completions = {
             completeFunctionCalls = true,
           },
