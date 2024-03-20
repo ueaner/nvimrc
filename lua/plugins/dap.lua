3792c6e7de4121b3c769f9b35ec853952cb6f3d9
ueaner <ueaner@gmail.com>
2024-03-20 08:27:33 +0800

    fix: add required nvim-nio to test and dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -15,6 +15,7 @@ return {
       },
       {
         "rcarriga/nvim-dap-ui",
+        dependencies = { "nvim-neotest/nvim-nio" },
         config = function()
           require("dapui").setup()
         end,
--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -4,6 +4,7 @@ return {
     "nvim-neotest/neotest",
     event = "VeryLazy",
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       {
