7eb8f60b0dba831de25d8a83e973302dd7be3268
ueaner <ueaner@gmail.com>
2024-04-19 18:08:20 +0800

    chore(typescript): update comments

--- lua/plugins/extras/lang/typescript.lua
+++ lua/plugins/extras/lang/typescript.lua
@@ -84,7 +84,7 @@ local conf = {
               command = "node",
               -- 💀 Make sure to update this path to point to your installation
               args = {
-                -- ~/.local/share/nvim/mason/packages/js-debug-adapter
+                -- ~/.local/share/nvim/mason/bin/js-debug-adapter
                 require("mason-registry").get_package("js-debug-adapter"):get_install_path()
                   .. "/js-debug/src/dapDebugServer.js",
                 "${port}",
