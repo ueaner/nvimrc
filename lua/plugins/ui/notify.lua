cd648d5e65b9143b827e2439d58a4f2286f1d1ef
ueaner <ueaner@gmail.com>
2024-05-16 23:16:50 +0800

    feat(notify): added keymap `<leader>snt` to show Noice messages in telescope

--- lua/plugins/ui/notify.lua
+++ lua/plugins/ui/notify.lua
@@ -39,6 +39,7 @@ return {
       { "<leader>snh", function() require("noice").cmd("history") end, desc = "Noice History" },
       { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
       { "<leader>snd", function() require("noice").cmd("dismiss") end, desc = "Dismiss All" },
+      { "<leader>snt", function() require("noice").cmd("telescope") end, desc = "Noice Telescope" },
 
       { "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<Right>" end end, silent = true, expr = true, desc = "Scroll forward", mode = {"i"}},
       { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<Left>" end end, silent = true, expr = true, desc = "Scroll backward", mode = {"i"}},
