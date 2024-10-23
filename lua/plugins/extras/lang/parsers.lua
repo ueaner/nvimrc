bdeee430154f0872e37ba54d5ca68e1f471cc698
ueaner <ueaner@gmail.com>
2024-10-24 01:30:11 +0800

    feat(treesitter): add more parsers

---
+++ lua/plugins/extras/lang/parsers.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "java",
+      "groovy",
+      "kotlin",
+      "xml",
+      "swift",
+      "nginx",
+    },
+  },
+}
