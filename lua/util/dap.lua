273a2f3fa356440d3efe3a93ac448465c7e81463
ueaner <ueaner@gmail.com>
2024-11-12 18:39:33 +0800

    fix(dap): use dap's splitstr when running with args

--- lua/util/dap.lua
+++ lua/util/dap.lua
@@ -6,7 +6,7 @@ function M.get_args()
   return coroutine.create(function(dap_run_co)
     local args = {}
     vim.ui.input({ prompt = "Args: " }, function(input)
-      args = vim.split(input or "", " ")
+      args = require("dap.utils").splitstr(input or "")
       coroutine.resume(dap_run_co, args)
     end)
   end)
