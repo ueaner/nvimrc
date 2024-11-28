2b1bc5b97a37538cbbd6b24c9d5e06c2d5959849
ueaner <ueaner@gmail.com>
2024-11-28 17:30:37 +0800

    fix(plantuml): use `~/bin/plantuml` to make plantuml file to image

--- lua/plugins/extras/tools/plantuml.lua
+++ lua/plugins/extras/tools/plantuml.lua
@@ -2,7 +2,13 @@ return {
   -- plantuml syntax
   {
     "aklt/plantuml-syntax",
-    ft = { "plantuml", "markdown" },
+    ft = { "plantuml" },
+    init = function()
+      vim.g["plantuml_set_makeprg"] = 0
+      -- https://github.com/ueaner/dotfiles/blob/main/bin/plantuml
+      vim.opt_local["makeprg"] = vim.env.HOME .. "/bin/plantuml %"
+      vim.opt_local["errorformat"] = "Error line %l in file: %f,%Z%m"
+    end,
   },
 
   -- plantuml preview in browser
@@ -18,6 +24,7 @@ return {
       "PlantumlSave",
     },
     init = function()
+      -- https://github.com/ueaner/dotfiles/blob/main/ansible/roles/tools/tasks/plantuml.yml
       vim.g["plantuml_previewer#plantuml_jar_path"] = vim.env.XDG_BIN_HOME .. "/plantuml.jar"
     end,
   },
