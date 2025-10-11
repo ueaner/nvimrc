6657f4e5701dc4c92fee86520295235f7d7cf814
ueaner <ueaner@gmail.com>
2025-10-12 04:25:34 +0800

    chore(python): dap-python with uv

--- lua/plugins/extras/lang/python.lua
+++ lua/plugins/extras/lang/python.lua
@@ -51,7 +51,8 @@ local conf = {
     {
       "mfussenegger/nvim-dap-python",
       config = function()
-        require("dap-python").setup("~/.local/share/nvim/mason/packages/debugpy/venv/bin/python")
+        -- https://github.com/mfussenegger/nvim-dap-python#usage
+        require("dap-python").setup("uv")
       end,
     },
   },
