da52815b9b3a3ed02a16be41cdbc20ad671093dc
ueaner <ueaner@gmail.com>
2025-11-21 18:01:52 +0800

    feat(colorizer): Highlight the hex codes `#RRGGBBAA`

--- lua/plugins/extras/tools/colorizer.lua
+++ lua/plugins/extras/tools/colorizer.lua
@@ -10,13 +10,13 @@ return {
       user_default_options = {
         RGB      = true,  -- #RGB hex codes
         RRGGBB   = true,  -- #RRGGBB hex codes
-        names    = false, -- "Name" codes like Blue
-        RRGGBBAA = false, -- #RRGGBBAA hex codes
+        names    = true,  -- "Name" codes like Blue
+        RRGGBBAA = true,  -- #RRGGBBAA hex codes
         rgb_fn   = false, -- CSS rgb() and rgba() functions
         hsl_fn   = false, -- CSS hsl() and hsla() functions
         css      = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
         css_fn   = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        tailwind = true,  -- Enable tailwind colors
+        -- Available modes: foreground, background
         mode     = "background", -- Set the display mode.
       },
     },
